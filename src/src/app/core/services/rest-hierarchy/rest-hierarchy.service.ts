9aa5e667569e369ef32709f9bef19b680ad2f616
Aaron Scribner <aaron.scribner@braingrape.com>
2020-01-23 13:06:40 -0700

    Fixed the detecting of subresources

--- src/src/app/core/services/rest-hierarchy/rest-hierarchy.service.ts
+++ src/src/app/core/services/rest-hierarchy/rest-hierarchy.service.ts
@@ -6,11 +6,17 @@ import { SubResource } from '../../../shared/models/base-classes/sub-resource.mo
 })
 export class RestHierarchyService {
   public static assignHierarchyIdentifiers(subResource: SubResource | any, parentIdentifiers: any): void {
-    const composedSubResources = Object.entries(subResource)
+    let composedSubResources = Object.entries(subResource)
       .filter(([key, value]) => key !== 'identifierHierarchy' && value.constructor === Array)
-      .map(([key, value]) => Object.values(value).reduce(x => x));
+      .map(([key, value]) => Object.values(value));
 
     subResource.identifierHierarchy = parentIdentifiers;
+
+    if (!composedSubResources || composedSubResources.length === 0) {
+      return;
+    }
+
+    composedSubResources = composedSubResources.reduce((acc, value) => [acc, ...value]);
     console.table(composedSubResources);
     const identifiers = [...parentIdentifiers, subResource.id];
     composedSubResources.forEach(x => RestHierarchyService.assignHierarchyIdentifiers(x, identifiers));
