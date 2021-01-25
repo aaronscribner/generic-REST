19675fc72ee6ecc95654f5a0532c447f41f3b56f
Aaron Scribner <aaron.scribner@braingrape.com>
2021-01-25 11:44:27 -0700

    Ng 11 Material upgrade

--- src/src/app/ancestry/ancestry.module.ts
+++ src/src/app/ancestry/ancestry.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AncestryRoutingModule } from './ancestry-routing.module';
 import { AncestryComponent } from './ancestry.component';
