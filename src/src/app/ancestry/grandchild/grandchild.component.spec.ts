27ea4df03f25aa70850d2a9b9d903aff534bfb5b
Aaron Scribner <aaron.scribner@braingrape.com>
2021-01-25 11:35:11 -0700

    Ng 11 CDK update

--- src/src/app/ancestry/grandchild/grandchild.component.spec.ts
+++ src/src/app/ancestry/grandchild/grandchild.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GrandchildComponent } from './grandchild.component';
 
@@ -6,7 +6,7 @@ describe('RootGrandchildComponent', () => {
   let component: GrandchildComponent;
   let fixture: ComponentFixture<GrandchildComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GrandchildComponent ]
     })
